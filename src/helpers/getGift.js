a747684fda3604779bf7d39358ab5c9c207ff45a
unknown <user@oficp-ababdev.MAGNA_DOMAIN.com>
2021-04-03 23:04:51 -0400

    docs update

--- src/helpers/getGift.js
+++ src/helpers/getGift.js
@@ -1,6 +1,6 @@
 export const getGifts = async(category) =>{
     const api_key ='bBem1QwqYR0BrKewQd7NwDiGr6LfFktF';
-    const url_get = 'http://api.giphy.com/v1/gifs/search';
+    const url_get = 'https://api.giphy.com/v1/gifs/search';
     //category = 'goku';
     const url =`${url_get}?q=${category}&api_key=${api_key}&limit=10`;
     console.log(url);
